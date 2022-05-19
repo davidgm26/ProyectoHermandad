b481435bd81f26cb826d88d254e4eb5749599a44
David Garcia <davidgama260402@gmail.com>
2022-05-19 13:50:57 +0200

    Borro todo el master para hacer merge

--- ProyectoHermandadEstrella/src/main/java/com/salesianostriana/dam/proyectohermandadestrella/repositorio/HermanoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectohermandadestrella.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectohermandadestrella.model.Hermano;
-
-public interface HermanoRepositorio extends JpaRepository <Hermano, Long>{
-
-}
