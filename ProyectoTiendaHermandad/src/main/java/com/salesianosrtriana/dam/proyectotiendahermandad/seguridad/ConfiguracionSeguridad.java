5d30baf475999a9241c93ad9b3924e4f983b5d27
David Garcia <davidgama260402@gmail.com>
2022-05-20 09:30:27 +0200

    Encabezado de Editar

--- ProyectoTiendaHermandad/src/main/java/com/salesianosrtriana/dam/proyectotiendahermandad/seguridad/ConfiguracionSeguridad.java
+++ ProyectoTiendaHermandad/src/main/java/com/salesianosrtriana/dam/proyectotiendahermandad/seguridad/ConfiguracionSeguridad.java
@@ -1,29 +1,61 @@
 package com.salesianosrtriana.dam.proyectotiendahermandad.seguridad;
-/*
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import com.salesianostriana.dam.proyectotiendahermandad.modelo.Usuario;
-import com.salesianostriana.dam.proyectotiendahermandad.repositorio.UsuarioRepo;
-
 @Configuration
-@EnableWebSecurity
-public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
-
+/*@EnableWebSecurity*/
+public class ConfiguracionSeguridad /*extends WebSecurityConfigurerAdapter*/{
+}/*
+	private List<Usuario>usuarios;
+	
     @Autowired
     private UsuarioRepo usuarioRepo;
 
-  /*  @Override
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
-    }*/
- /*   
-    private List<Usuario>usuarios;
+    }
+ 
+    
 
-}
-*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/private/**").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().permitAll()
+            .and().exceptionHandling().accessDeniedPage("/error")
+            .and().formLogin().loginPage("/").loginProcessingUrl("/login").failureUrl("/login-error").permitAll()
+            .and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll();
+
+    }
+    
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        usuarios.getUsuarios()
+                .stream()
+                .map(u -> {
+                    return User
+                            .withUsername(u.getUsername())
+                            .password("{noop}"+ u.getPassword())
+                            .roles(u.getRole())
+                            .build();
+
+                })
+                .forEach(userDetailsManager::createUser);
+
+
+        return userDetailsManager;
+
+
+    }
+}*/
