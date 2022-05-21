d2def74d159e15f6e37583a063dc4f68182baf63
davidgm26 <davidgama260402@gmail.com>
2022-05-21 17:35:55 +0200

    Seguridad incompleta

--- ProyectoTiendaHermandad/src/main/java/com/salesianosrtriana/dam/proyectotiendahermandad/seguridad/ConfiguracionSeguridad.java
+++ ProyectoTiendaHermandad/src/main/java/com/salesianosrtriana/dam/proyectotiendahermandad/seguridad/ConfiguracionSeguridad.java
@@ -10,25 +10,47 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import com.salesianostriana.dam.proyectotiendahermandad.repositorio.UserRepo;
-
+import com.salesianostriana.dam.proyectotiendahermandad.repositorio.UsuarioRepo;
+import com.salesianostriana.dam.proyectotiendahermandad.modelo.Usuario;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
 	
     @Autowired
-    private UserRepo usuarioRepo;
+    private UsuarioRepo usuarioRepo;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
     }
  
-    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	 	http.csrf().disable().authorizeRequests()
+			.antMatchers("/admin")
+			.hasRole("ADMIN")
+			  .antMatchers("/","index","/cristo","/virgen","/juntaDeGobierno","/reglas",
+		                "musica","/recorrido")
+			.hasAnyRole("ADMIN").anyRequest().permitAll().and().exceptionHandling()
+			.accessDeniedPage("/login").and().formLogin().loginPage("/login").loginProcessingUrl("/login")
+			.failureUrl("/login-error").permitAll().and().formLogin().defaultSuccessUrl("/menu-principal").and()
+			.logout().logoutUrl("/logout").logoutSuccessUrl("/inicio").permitAll().and().headers().frameOptions()
+			.disable();
+}
+ /*      
+                .authorizeRequests()
+                .antMatchers("/","index","/cristo","/virgen","/juntaDeGobierno","/reglas",
+                        "musica","/recorrido").permitAll()
+                .antMatchers("/","index","/cristo","/virgen","/juntaDeGobierno","/reglas",
+                "musica","/recorrido").hasAnyRole("USER", "ADMIN")
+                .and().exceptionHandling().accessDeniedPage("/error")
+                .and().formLogin()
+                .and().logout().logoutSuccessUrl("/");
 
-
+    }
+*/
+/*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -42,7 +64,7 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
             .and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll();
 
     }
-    
+*/
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
